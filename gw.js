403557c9e3a5be5694fe55df7ad2cee0dc16414b
Barduino <barduinor@gmail.com>
2016-02-01 15:36:08 -0500

    Clean up

--- gw.js
+++ gw.js
@@ -1,37 +1,3 @@
-// SETTINGS
-var    KEY = 'anpj63w4mm6wk8tasmyfww9mw';
-var SECRET = 'ryaykwoamnddbjh4trjeihxvxhc365hi3me8p65qlq1og1k6le';
-var   HOST = 'barduino.outsystemscloud.com';
-
-var mqtt_host = 'mqtts://m10.cloudmqtt.com';
-var mqtt_port = 20116
-var mqtt_username = 'xpxjokix';
-var mqtt_password = 'muWRRbZs9hoW';
-// mqtt client options
-var mqtt_Options = {
-     port:mqtt_port
-    ,keepalive: 10 //seconds, set to 0 to disable
-    ,clientId: 'haall' + KEY
-    ,protocolId: 'MQTT'
-    //,protocolVersion: 4
-    ,clean: false //set to false to receive QoS 1 and 2 messages while offline
-    ,reconnectPeriod: 1000 // milliseconds, interval between two reconnections
-    ,connectTimeout: 30 * 1000  //milliseconds, time to wait before a CONNACK is received
-    ,username:mqtt_username //the username required by your broker, if any
-    ,password:mqtt_password//the password required by your broker, if any
-    /*
-    ,incomingStore: , // a Store for the incoming packets
-    ,outgoingStore: , // a Store for the outgoing packets
-    ,will: { //a message that will sent by the broker automatically when the client disconnect badly. The format is:
-        topic: , // the topic to publish
-        payload:, // the message to publish
-        qos: , //the QoS
-        retain: // the retain flag 
-    }
-    */
-}
-
-
 // LIBS
 var com = require("serialport");
 var http = require('http');
@@ -40,35 +6,77 @@ var crypto = require('crypto');
 var sleep = require('sleep');
 var mqtt = require('mqtt');
 
+// CONTROLLER SETTINGS
+var    KEY = 'anpj63w4mm6wk8tasmyfww9mw';
+var SECRET = 'ryaykwoamnddbjh4trjeihxvxhc365hi3me8p65qlq1og1k6le';
+var   HOST = 'barduino.outsystemscloud.com';
 
+// SERIAL PORT SETTINGS
+var serial_port = '/dev/ttyACM0';
+
+// REST SERVER SETTINGS
+var rest_port = 7070;
+var rest_host = '0.0.0.0';
+
+// MQTT SETTINGS
+var mqtt_host     = 'mqtts://m10.cloudmqtt.com';
+var mqtt_port     =  20116
+var mqtt_username = 'xpxjokix';
+var mqtt_password = 'muWRRbZs9hoW';
+
+// OTHER SETTINGS
 var SHORT_WAIT = 500000; //microseconds (1 second is 1000000 microseconds)
+
+// INIT AUTHENTICATION
 var sha256 = crypto.createHash('sha256').update(KEY+':'+SECRET).digest("hex");
 console.log('Authorization:'+sha256);
 
-var serialPort = new com.SerialPort('/dev/ttyACM0', {
+// REST OPTIONS
+var rest_postheaders = {
+    'Content-Type'  : 'text/xml',
+    'ControllerKey' : KEY,
+	'Authorization' : sha256
+};
+var rest_postoptions = {
+    host : HOST,
+    port : 443,
+    path : '/api/rest/barduino/receive',
+    method : 'POST',
+    headers : rest_postheaders
+};
+
+// MQTT CLIENT OPTIONS
+var mqtt_Options = {
+     port:              mqtt_port
+    ,keepalive:         10                  //seconds, set to 0 to disable
+    ,clientId:          'haall_' + KEY
+    ,protocolId:        'MQTT'
+    //,protocolVersion: 4
+    ,clean:             false               //set to false to receive QoS 1 and 2 messages while offline
+    ,reconnectPeriod:   1000                // milliseconds, interval between two reconnections
+    ,connectTimeout:    30 * 1000           //milliseconds, time to wait before a CONNACK is received
+    ,username:          mqtt_username       //the username required by your broker, if any
+    ,password:          mqtt_password       //the password required by your broker, if any
+    /*
+    ,incomingStore: , // a Store for the incoming packets
+    ,outgoingStore: , // a Store for the outgoing packets
+    */
+    //a message that will sent by the broker automatically when the client disconnect badly. The format is:
+    ,will: {topic:  '/haall/'+KEY+'/out',                           // the topic to publish
+            payload:'Client haall_' + KEY +'has lost connection',   // the message to publish
+            qos:    1,                                              //the QoS
+            retain: false                                           // the retain flag 
+    }    
+}
+
+// SERIAL PORT
+var serialPort = new com.SerialPort(serial_port, {
     baudrate: 115200,
 	parser: com.parsers.readline('\n')
   });
  
-
-var mqtt_client  = mqtt.connect(mqtt_host,mqtt_Options);
-
-mqtt_client.subscribe('/haall/'+KEY+'/in/#');
-
-mqtt_client.on('message', function (topic, message) {
-  // message is Buffer 
-	var m = topic.toString();
-	m = m.replace('/haall/'+KEY+'/in/','');
-	m = m.split('/').join(';');
-	m=m+';'+message.toString();
-	serialPort.write(''+m+'\n');
-
-  	console.log('MQTT INCOMING: '+topic+':'+message.toString());
-	console.log('To MySensors: '+m);
-});
-mqtt_client.publish('/haall/'+KEY+'/out', 'HAALL mqtt client started at '+ new Date());
-
-server = http.createServer( function(req, res) {
+// REST INCOMING
+var rest_server = http.createServer( function(req, res) {
 
     //console.log(req.param);
 
@@ -89,7 +97,7 @@ server = http.createServer( function(req, res) {
 					var j;
 					j = body.indexOf('\n',i);
 					if (j == -1) j = body.length;
-					console.log('INCOMING yy: '+body.substr(i, j-i));
+					console.log('INCOMING REST: '+body.substr(i, j-i));
 					serialPort.write(''+body.substr(i, j-i)+'\n');
 					i = j+1;
 					sleep.usleep(SHORT_WAIT);
@@ -113,67 +121,50 @@ server = http.createServer( function(req, res) {
 
 });
 
-function authenticate(controllerKey,authorization){
-	var testAuth = crypto.createHash('sha256').update(controllerKey+':'+SECRET).digest("hex");
-	return testAuth == authorization;
-}
-
-port = 7070;
-host = '0.0.0.0';
-server.listen(port, host);
-console.log('Listening at http://' + host + ':' + port);
- 
-
-
-var postheaders = {
-    'Content-Type' : 'text/xml',
-    'ControllerKey' : KEY,
-	'Authorization' : sha256
-};
-  
-var optionspost = {
-    host : HOST,
-    port : 443,
-    path : '/api/rest/barduino/receive',
-    method : 'POST',
-    headers : postheaders
-};
-
-console.log('Opening Serial port');
-
+// START SERIAL PORT 
+console.log('Opening Serial port...');
 serialPort.open(function (error) {
   if ( error ) {
     console.log('failed to open: '+error);
   } else {
-    console.log('open');
+    console.log('Serial port opened!');
   }
 });
 
-//console.log('Serial port opened');
+// START REST
+console.log('Starting REST...');
+rest_server.listen(rest_port, rest_host);
+console.log('Listening at http://' + rest_host + ':' + rest_port);
+ 
+// START MQTT
+console.log('Starting MQTT...'); 
+var mqtt_client  = mqtt.connect(mqtt_host,mqtt_Options);
+console.log('Subscribing MQTT...'); 
+mqtt_client.subscribe('/haall/'+KEY+'/in/#');
+console.log('Publish MQTT...'); 
+mqtt_client.publish('/haall/'+KEY+'/out', 'HAALL mqtt client started at '+ new Date());
 
+// SERIAL PORT OUTGOING
 serialPort.on('data', function(data) {
 	
 	if (data.indexOf('0;0;3;0;9;') == 0) {
-		console.log('	'+data);
-		//Comment next line to Ignore I_MESSAGE_LOG
-		execREST(data);
-		mqttPublish(data);
+		console.log('        I_LOG: '+data);
+        mqttPublish(data);
+		execREST(data);		
 	}
 	else{
 		console.log(data);
 		mqttPublish(data);
 		execREST(data);
 	}
-
 });
 
+// REST OUTGOING
 function execREST(post_content){
-	var reqPost = https.request(optionspost, function(res) {
+	var reqPost = https.request(rest_postoptions, function(res) {
 		var resp='';
     	res.on('data', function (chunk) {
 			resp += chunk;
-			// console.log('INCOMING: '+chunk);
-			// serialPort.write(''+chunk.substr+'\n');
 		});
 		res.on('end', function(){
 			var i = 0;
@@ -182,7 +173,7 @@ function execREST(post_content){
 				var j;
 				j = resp.indexOf('\n',i);
 				if (j == -1) j = resp.length;
-				console.log('INCOMING xx: '+resp.substr(i, j-i));
+				console.log('OUTGOING REST: '+resp.substr(i, j-i));
 				serialPort.write(''+resp.substr(i, j-i)+'\n');
 				i = j+1;
 				sleep.usleep(SHORT_WAIT);
@@ -198,6 +189,18 @@ function execREST(post_content){
 	});
 }
 
+// MQTT INCOMING
+mqtt_client.on('message', function (topic, message) {
+	var m = topic.toString();
+	m = m.replace('/haall/'+KEY+'/in/','');
+	m = m.split('/').join(';');
+	m = m + ';' + message.toString();
+	serialPort.write('' + m + '\n');
+
+  	console.log('INCOMING MQTT: ' + topic + ':' + message.toString());
+});
+
+// MQTT OUTGOING
 function mqttPublish(data){
 	var topic = '/haall/'+KEY+'/out/';
 	var params = data.split(';');
@@ -208,6 +211,11 @@ function mqttPublish(data){
 	topic = topic + params[4] + '';
 	var payload = params[5];
 	mqtt_client.publish(topic,payload);
-	//console.log('MQTT publish: '+topic + ' Payload: '+ payload);
+	console.log('OUTGOING MQTT: ' + topic + ' Payload: ' + payload);
 }
 
+// AUTHENICATION
+function authenticate(controllerKey,authorization){
+	var testAuth = crypto.createHash('sha256').update(controllerKey+':'+SECRET).digest("hex");
+	return testAuth == authorization;
+}
