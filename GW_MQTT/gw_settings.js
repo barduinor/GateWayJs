e944975d5fa4161083623ea10afae74e0a1d8c2d
Barduino <barduinor@gmail.com>
2016-02-04 23:46:29 -0500

    refactor, mqtt

---
+++ GW_MQTT/gw_settings.js
@@ -0,0 +1,8 @@
+// MQTT SETTINGS
+var mqtt_host     = 'mqtts://m10.cloudmqtt.com';
+var mqtt_port     =  29839
+var mqtt_username = 'kknntiab';
+var mqtt_password = 'O6zi0W_1o0sf';
+
+// CONTROLLER SETTINGS
+var    KEY = 'anpj63w4mm6wk8tasmyfww9mw';
